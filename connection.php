b5c0cf1d30574d87109946a2895e1b51dc702ab2
enevlogiev <edward.evlogiev@gmail.com>
2015-04-21 17:24:47 +0300

    initial

---
+++ connection.php
@@ -0,0 +1,57 @@
+<?php
+    $host = "sql3.freemysqlhosting.net";
+    $dbname = "sql374012";
+    $user= "sql374012";
+    $pass = "hQ1*kD8%";
+    $port = "3306";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
+
+if (!$conn) {
+    die("error");
+}
+//echo "success<br>";
+
+//$sql = "CREATE TABLE Users (
+//    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//username VARCHAR(30) NOT NULL,
+//password VARCHAR(64) NOT NULL,
+//reg_date TIMESTAMP
+//)";
+//
+////$query = mysqli_query($conn, $sql);
+////
+////if ($query) {
+////    echo "successfully created";
+////} else {
+////    echo "failed";
+////}
+//
+//$date = time();
+//$name = "Gosho";
+//$pass = md5("pesho");
+//
+////$query = sprintf('INSERT INTO Users (username, password, reg_date)
+////VALUES ("%s", "%s", "%s")', mysqli_real_escape_string($conn, $name),
+////    mysqli_real_escape_string($conn, $pass),
+////    mysqli_real_escape_string($conn, $date));
+////
+////$result = mysqli_query($conn, $query);
+////
+////if ($result) {
+////    echo "inserted";
+////} else {
+////    echo mysqli_error($conn);
+//
+//
+//$query = "SELECT * FROM Users";
+//$result = mysqli_query($conn, $query);
+//
+//
+//
+//if ($result) {
+//    $output = mysqli_fetch_assoc($result);
+//    var_dump($result);
+//} else {
+//    echo mysqli_error($conn);
+//}
---
+++ register.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<form action="register.php" method="post">
+    <input type="text" name="username"/>
+    <input type="password" name="password"/>
+    <input type="password" name="rpassword"/>
+    <input type="submit" name="submit"/>
+</form>
+<?php
+    include_once('connection.php');
+if (isset($_POST['submit'])) {
+    $pass = md5(htmlspecialchars($_POST['password']));
+    $rpass = md5(htmlspecialchars($_POST['rpassword']));
+    $uname = htmlspecialchars($_POST['username']);
+    if ($pass != $rpass) {
+        die("Passwords do not match");
+    }
+    var_dump($uname);
+    $uname = mysqli_real_escape_string($conn, $uname);
+    $queryUser = "SELECT username FROM Users WHERE username = '$uname';";
+    $selected = mysqli_query($conn, $queryUser);
+    if ($selected->num_rows) {
+        echo "user exists";
+    } else {
+        $date = new DateTime();
+        $query = sprintf('INSERT INTO Users (username, password, reg_date)
+                        VALUES ("%s", "%s", "%s")', mysqli_real_escape_string($conn, $uname),
+            mysqli_real_escape_string($conn, $pass),
+            mysqli_real_escape_string($conn, $date->getTimestamp()));
+
+        $result = mysqli_query($conn, $query);
+        var_dump($result);
+    }
+}
+?>
+</body>
+</html>
+
+
+
+<?php
