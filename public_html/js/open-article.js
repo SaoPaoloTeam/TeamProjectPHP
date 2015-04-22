ba3203750dd7675ee18800138007e1ce1a9c0848
Gdamianov <georgi_damianov93@abv.bg>
2015-04-22 20:02:56 +0300

    open-article

--- public_html/js/open-article.js
+++ public_html/js/open-article.js
@@ -1,3 +1,19 @@
-/**
- * Created by Georgi on 22/04/2015.
- */
+
+function takeArticleId(clicked_article){
+
+    var article =  document.getElementById(clicked_article);
+    var parent = document.getElementById("content");
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+    article.style.cursor ="auto"
+    article.style.visibility ="visible";
+    article.style.height = "auto";
+    parent.appendChild(article);
+    var fullArticle = document.getElementById(clicked_article).children;
+    fullArticle[1].style.overflow = "visible";
+    fullArticle[1].style.height ="auto";
+    var atag = fullArticle[2].children;
+    console.log(atag[2]);
+    atag[2].style.visibility = "visible";
+}
