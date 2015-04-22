8b5af8464ad3f521895a7c18719e97eed00ecc1c
vesheff <martin.veshev@gmail.com>
2015-04-22 15:15:29 +0300

    Added some useful comments

--- resources/custom_functions.php
+++ resources/custom_functions.php
@@ -1,21 +1,19 @@
 <?php
 require_once("config.php");
 
-
+//Redirects to location of choice
 function redirect_to($new_location)
 {
     header('Location:' . $new_location);
     exit;
 }
 
-
+//Doing mysqli_real_escape_string() to multiple data
+//$arr is the array passed to escape and $conn is the default connection from config.php
 function escapeAll($arr,$conn){
-
     $finalArr = [];
     foreach ($arr as $value) {
         $finalArr[$value] = mysqli_real_escape_string($conn,$value);
     }
-
     return $finalArr;
-
 }
