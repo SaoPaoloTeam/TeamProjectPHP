438cb5c6e427e2ca328fb82309424c817ad868f7
enevlogiev <edward.evlogiev@gmail.com>
2015-04-24 17:30:33 +0300

    article styling added

--- resources/templates/head.php
+++ resources/templates/head.php
@@ -9,7 +9,6 @@
 
     <link href='css/reset.css' rel='stylesheet'>
     <link href='css/main.css' rel='stylesheet'>
-    <link href="css/temp.css" rel="stylesheet"/>
     <script src="../resources/library/jquery-2.1.3.js"></script>
     <script src="js/functions.js"></script>
     <script src="js/user-functions.js"></script>
