b5054bf53523201501b4e5383a2b9b1689354bd3
enevlogiev <edward.evlogiev@gmail.com>
2015-04-24 21:16:58 +0300

    fixes to header styling

--- resources/templates/headerLoggedIn.php
+++ resources/templates/headerLoggedIn.php
@@ -1,11 +1,15 @@
 
 <header>
     <nav>
-
+        <section class="logo">
+            A Blog Of Ice And Fire
+        </section>
+        <section>
         <div class="hello-box">
             <?php echo "Hello," . $_SESSION['user']; ?>
         </div>
         <a href='../resources/app_controls/logout.php' class="user-logout">Logout</a>
+        </section>
     </nav>
     <section class="bg-holder">
 
