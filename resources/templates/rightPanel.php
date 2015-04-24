4e392a84205a43480793938891995d7017d5d3fc
enevlogiev <edward.evlogiev@gmail.com>
2015-04-24 13:59:33 +0300

    added sorting by tags

--- resources/templates/rightPanel.php
+++ resources/templates/rightPanel.php
@@ -1,11 +1,12 @@
 
 <aside>
     <ul>
-        <li><a href="#">The Night's Watch</a></li>
-        <li><a href="#">The North</a></li>
-        <li><a href="#">The Iron Islands</a></li>
-        <li><a href="#">Dorne</a></li>
-        <li><a href="#">Slaver's Bay</a></li>
-        <li><a href="#">The Free Cities</a></li>
+        <li><a href="index.php?page=0&cat=all">All Articles</a></li>
+        <li><a href="index.php?page=0&cat=nightwatch">The Night's Watch</a></li>
+        <li><a href="index.php?page=0&cat=north">The North</a></li>
+        <li><a href="index.php?page=0&cat=iron">The Iron Islands</a></li>
+        <li><a href="index.php?page=0&cat=dorne">Dorne</a></li>
+        <li><a href="index.php?page=0&cat=slaver">Slaver's Bay</a></li>
+        <li><a href="index.php?page=0&cat=freecities">The Free Cities</a></li>
     </ul>
 </aside>
